3c72e30a9b12cd6030dee7c7be507735fc73acb1
Zakir Hussain <zakir.nevadia@gmail.com>
2022-01-02 00:36:12 +0600

    Add basic CRUD endpoints.

--- src/main/java/com/nextzakir/springdataneo4jrestapi/repository/PersonRepository.java
+++ src/main/java/com/nextzakir/springdataneo4jrestapi/repository/PersonRepository.java
@@ -3,5 +3,8 @@ package com.nextzakir.springdataneo4jrestapi.repository;
 import com.nextzakir.springdataneo4jrestapi.node.Person;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 
+import java.util.Optional;
+
 public interface PersonRepository extends Neo4jRepository<Person, Long> {
+    Optional<Person> findByPersonId(Long personId);
 }
