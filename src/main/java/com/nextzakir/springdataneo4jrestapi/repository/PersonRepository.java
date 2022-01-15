b234e58dae66bb06de0ace4b3187d1f777fa8f29
Zakir Hussain <zakir.nevadia@gmail.com>
2022-01-15 16:08:27 +0600

    Update on 15/01/2022 again.

--- src/main/java/com/nextzakir/springdataneo4jrestapi/repository/PersonRepository.java
+++ src/main/java/com/nextzakir/springdataneo4jrestapi/repository/PersonRepository.java
@@ -2,6 +2,13 @@ package com.nextzakir.springdataneo4jrestapi.repository;
 
 import com.nextzakir.springdataneo4jrestapi.node.Person;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface PersonRepository extends Neo4jRepository<Person, Long> {
+
+    Optional<Person> findByPersonId(Long personId);
+
 }
